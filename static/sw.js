6b6efb3d103a887fd68b524ed246c72e947948c3
azh2000xy <azh2000xy@gmail.com>
2026-06-14 11:24:16 +0800

    Add PWA support + Render deployment config

---
+++ static/sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'juice-n-cook-v1';
+const ASSETS = [
+  '/',
+  '/static/manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
